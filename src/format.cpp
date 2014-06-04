984203da3397160e841bac12be990170f20b9624
Jamboree <tongari95@gmail.com>
2014-06-04 20:45:39 +0800

    more optimize/use 'until'

--- src/format.cpp
+++ src/format.cpp
@@ -15,24 +15,19 @@ namespace bustache { namespace parser
     using x3::space;
     using x3::char_;
     using x3::string;
-    using x3::alpha;
-    using x3::alnum;
-    using x3::eps;
-    using x3::attr;
     
     using x3::seek;
-    using x3::sink;
+    using x3::lexeme;
     using x3::skip;
     using x3::raw;
-    using x3::locals;
+    using x3::until;
 
     x3::rule<class start, ast::content_list> const start;
-    x3::rule<class text, boost::string_ref, x3::filter<>> const text;
-    x3::rule<class id, std::string, x3::filter<>> const id;
+    x3::rule<class text, boost::string_ref> const text;
+    x3::rule<class id, std::string> const id;
     x3::rule<class term, std::string> const term;
     x3::rule<class block, ast::block> const block;
     x3::rule<class content, ast::content> const content;
-    x3::rule<class end_block, bool(std::string const&)> const end_block;
     
     x3::as<boost::string_ref> const as_text;
     
@@ -45,38 +40,30 @@ namespace bustache { namespace parser
         }
     };
 
-    x3::param_eval<0> const _r1;
-    x3::var_eval<0> const _a;
-
     BOOST_SPIRIT_DEFINE
     (
         start =
             *(content | as_text[string])
             
       , text =
-            raw[seek[char_ >> &skip(space)["{{"]]]
+            lexeme[raw[seek[char_ >> &skip["{{"]]]]
             
       , id =
-            raw[seek[char_ >> &skip(space)["}}"]]]
+            lexeme[raw[seek[char_ >> &skip["}}"]]]]
 
       , term =
             id >> "}}"
             
       , block =
                 '#' >> id >> "}}"
-            >>  locals<bool>()
+            >>  until(lit("{{") >> '/' >> lit(get_id()) >> "}}")
                 [
-                    *((sink(_a)[end_block(get_id())] | content) - eps(_a))
-                >>  eps(_a)
+                    content
                 ]
 
       , content =
                 "{{" >> (block | term)
             |   text
-
-      , end_block =
-                lit("{{") >> '/' >> lit(_r1) >> "}}"
-            >>  attr(true)
     )
 }}
 
